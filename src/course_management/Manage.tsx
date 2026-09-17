242fd0b24425c3a17c67311ab1a98ce9833f7dfb
Cydewyn <cydewyn@gmail.com>
2026-09-17 15:59:32 -0400

    Added mobile layout

--- src/course_management/Manage.tsx
+++ src/course_management/Manage.tsx
@@ -1,10 +1,16 @@
 import '../index.css';
+import {Button, Typography, ButtonGroup} from "@mui/material";
 
 // Material Imports
 //import Stack from '@mui/material/Stack';
 
 export default function Course_Management() {
       return (
+
+            <>
             <h1>Course Management</h1>
+            
+
+            </>
       )
 }
